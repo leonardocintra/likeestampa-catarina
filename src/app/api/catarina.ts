edb9845f9cbac028f92b7309ad2a4a802629e0ec
Leonardo Cintra <leonardo.ncintra@outlook.com>
2023-05-10 22:24:41 -0300

    feat: env

--- src/app/api/catarina.ts
+++ src/app/api/catarina.ts
@@ -7,9 +7,8 @@ export default async function getData<T>(url: string) {
     },
     headers: {
       "Content-Type": "application/json",
-      "firm-slug": "johnson-johnston-2b447612-b579-450d-922d-3121f794fc29",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjgsImVtYWlsIjoianVsaWFuYS5jaW50cmFAbHVpemFsYWJzLmNvbSIsImlhdCI6MTY4MzY2MjA4MiwiZXhwIjoxNjgzODM0ODgyfQ.EgxJIzbYzSqRS0LE3ite9u9A2wZ_BnIdJ7m-eplhaXQ",
+      "firm-slug": `${process.env.CATARINA_FIRM_ID}`,
+      Authorization: `Bearer ${process.env.CATARINA_API_TOKEN}`,
     },
   });
   return res.json();
