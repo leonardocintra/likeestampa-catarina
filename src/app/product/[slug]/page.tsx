eb8593c7769f98ffe1ac1970547c94d888239c02
Leonardo Cintra <leonardo.ncintra@outlook.com>
2023-06-03 14:24:53 -0300

    feat: product detail ok

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,6 +1,13 @@
 import Link from "next/link";
+import { IProduct } from "../../interfaces/IProduct";
+import catarinaGetData from "../../api/catarina";
 
-export default async function ProductDetail() {
+export default async function ProductDetail({ params }: any) {
+  const { slug } = params;
+  const product: IProduct = await catarinaGetData<IProduct>(
+    `/v1/catalogs/products/${slug}`
+  );
+
   return (
     <section className="text-gray-600 body-font overflow-hidden">
       <div className="container px-5 py-24 mx-auto">
@@ -12,10 +19,12 @@ export default async function ProductDetail() {
           />
           <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
             <h2 className="text-sm title-font text-gray-500 tracking-widest">
-              BRAND NAME
+              {product.categories
+                .filter((category) => category.parentId !== null)
+                .map((category) => category.name)}
             </h2>
             <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
-              The Catcher in the Rye
+              {product.name}
             </h1>
             <div className="flex mb-4">
               <span className="flex items-center">
@@ -115,14 +124,7 @@ export default async function ProductDetail() {
                 </Link>
               </span>
             </div>
-            <p className="leading-relaxed">
-              Fam locavore kickstarter distillery. Mixtape chillwave tumeric
-              sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo
-              juiceramps cornhole raw denim forage brooklyn. Everyday carry +1
-              seitan poutine tumeric. Gastropub blue bottle austin listicle
-              pour-over, neutra jean shorts keytar banjo tattooed umami
-              cardigan.
-            </p>
+            <p className="leading-relaxed">{product.description}</p>
             <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
               <div className="flex">
                 <span className="mr-3">Cor:</span>
@@ -157,10 +159,10 @@ export default async function ProductDetail() {
             </div>
             <div className="flex">
               <span className="title-font font-medium text-2xl text-gray-900">
-                R$ 58.00
+                {product.price}
               </span>
               <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
-                Comprar
+                Adicionar ao carrinho
               </button>
               <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
                 <svg
