e61aa02e93bd06d7e4ad148da0189bac44591cb4
Leonardo Cintra <leonardo.ncintra@outlook.com>
2023-06-03 13:26:00 -0300

    fix: fix link

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default async function ProductDetail() {
   return (
     <section className="text-gray-600 body-font overflow-hidden">
@@ -72,10 +74,10 @@ export default async function ProductDetail() {
                 >
                   <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                 </svg>
-                <span className="text-gray-600 ml-3">4 Reviews</span>
+                <span className="text-gray-600 ml-3">4 Avaliações</span>
               </span>
               <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
-                <a className="text-gray-500">
+                <Link href={"/"} className="text-gray-500">
                   <svg
                     fill="currentColor"
                     strokeLinecap="round"
@@ -86,8 +88,8 @@ export default async function ProductDetail() {
                   >
                     <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                   </svg>
-                </a>
-                <a className="text-gray-500">
+                </Link>
+                <Link href={"/"} className="text-gray-500">
                   <svg
                     fill="currentColor"
                     strokeLinecap="round"
@@ -98,8 +100,8 @@ export default async function ProductDetail() {
                   >
                     <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                   </svg>
-                </a>
-                <a className="text-gray-500">
+                </Link>
+                <Link href={"/"} className="text-gray-500">
                   <svg
                     fill="currentColor"
                     strokeLinecap="round"
@@ -110,7 +112,7 @@ export default async function ProductDetail() {
                   >
                     <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
                   </svg>
-                </a>
+                </Link>
               </span>
             </div>
             <p className="leading-relaxed">
@@ -123,19 +125,19 @@ export default async function ProductDetail() {
             </p>
             <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
               <div className="flex">
-                <span className="mr-3">Color</span>
+                <span className="mr-3">Cor:</span>
                 <button className="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
                 <button className="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
                 <button className="border-2 border-gray-300 ml-1 bg-indigo-500 rounded-full w-6 h-6 focus:outline-none"></button>
               </div>
               <div className="flex ml-6 items-center">
-                <span className="mr-3">Size</span>
+                <span className="mr-3">Tamanho</span>
                 <div className="relative">
                   <select className="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
-                    <option>SM</option>
+                    <option>P</option>
                     <option>M</option>
-                    <option>L</option>
-                    <option>XL</option>
+                    <option>G</option>
+                    <option>GG</option>
                   </select>
                   <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
                     <svg
@@ -155,10 +157,10 @@ export default async function ProductDetail() {
             </div>
             <div className="flex">
               <span className="title-font font-medium text-2xl text-gray-900">
-                $58.00
+                R$ 58.00
               </span>
               <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
-                Button
+                Comprar
               </button>
               <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
                 <svg
