64e2c05e020d271594fd024a5282a0c7f0204a80
Leonardo Cintra <leonardo.ncintra@outlook.com>
2024-03-27 20:08:22 -0300

    :wrench: foi o botao

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main>
       <h2>Like Estampa</h2>
       <p>Estamos reconstruindo a pagina</p>
+      <Button>Like estampa - Easy Panel</Button>
     </main>
   );
 }
