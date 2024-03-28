cd83d7f8d0ce17c880882bcb4f7fac790feae1c9
Leonardo Cintra <leonardo.ncintra@outlook.com>
2024-03-28 10:29:44 -0300

    :sparkles: test magalucloud

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,11 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main>
-      <h2>Like Estampa</h2>
-      <p>Estamos reconstruindo a pagina</p>
-      <Button>Like estampa - Easy Panel</Button>
+      <div className="flex flex-col max-w-lg mx-auto space-y-4">
+        <h2 className="text-center text-3xl">Like Estampa</h2>
+        <p className="font-mono text-lg text-center bg-green-200 p-2 rounded-lg hover:bg-green-400 transition-colors">Estamos reconstruindo a pagina</p>
+        <Button>Like estampa - Easy Panel</Button>
+      </div>
     </main>
   );
 }
--- src/components/custom/header.tsx
+++ src/components/custom/header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <div>
       <h1 className="text-gray-900 sm:text-2xl text-xl font-medium title-font mb-4 text-center py-2">
-        Like Estampa
+        Menu Header | Home | Login | Sobre | Contato
       </h1>
     </div>
   );
